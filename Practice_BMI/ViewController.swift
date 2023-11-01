3cc5e79d840c8c3a44a76d63cdd487363f051d16
Hyeonhee <hyunheeshim0103@gmail.com>
2023-11-01 23:58:17 +0900

    Initial Commit

---
+++ Practice_BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_BMI
+//
+//  Created by 심현희 on 2023/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
